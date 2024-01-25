1d79e2950bcc2672d0e01571e6a8e160a0aad7d4
lechatn <noe.lechat@ynov.com>
2024-01-25 08:28:05 +0100

    Changement de lecture pour l'Api. Lecture sur le lien principal.

--- main.go
+++ main.go
@@ -25,37 +25,72 @@ type dataApi struct {
 	Relations    string   `json:"relations"`
 }
 
-var jsonList []dataApi
+var jsonList_Artists []dataApi
 var artistData []string
-var nameArtist []string
+var generalData map[string]interface{}
 
 func main() {
 	css := http.FileServer(http.Dir("style"))                // For add css to the html pages
 	http.Handle("/style/", http.StripPrefix("/style/", css)) // For add css to the html pages
 	img := http.FileServer(http.Dir("images"))               // For add css to the html pages
 	http.Handle("/images/", http.StripPrefix("/images/", img))
-	url := "https://groupietrackers.herokuapp.com/api/artists"
-	response, err := http.Get(url)
+
+	url_General := "https://groupietrackers.herokuapp.com/api"
+	
+
+	////////////////////////////////////////////////////////////////////////////
+
+	response_General, err := http.Get(url_General)
 	if err != nil {
-		fmt.Println("Error")
+		fmt.Println("Error4")
+		return
+	}
+	defer response_General.Body.Close()
+
+	body_General, err := ioutil.ReadAll(response_General.Body)
+	if err != nil {
+		fmt.Println("Error5")
 		return
 	}
 
-	defer response.Body.Close()
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println("Error")
+	errUnmarshall := json.Unmarshal(body_General, &generalData)
+	if errUnmarshall != nil {
+		fmt.Println("Error6")
 		return
 	}
-	errUnmarshall := json.Unmarshal(body, &jsonList)
-	if errUnmarshall != nil {
-		fmt.Println("Error")
+
+	////////////////////////////////////////////////////////////////////////
+
+	url_Artists := generalData["artists"].(string)
+	//url_Locations := generalData["locations"].(string)
+	//url_Dates := generalData["dates"].(string)
+	//url_Relations := generalData["relation"].(string)
+
+
+/// ////////////////////////////////////////////////////////////////////Partie artsites
+	response_Artists, err := http.Get(url_Artists)
+	if err != nil {
+		fmt.Println("Error1")
+		return
+	}
+	defer response_Artists.Body.Close()
+
+
+
+	body_Artists, err := ioutil.ReadAll(response_Artists.Body)
+	if err != nil {
+		fmt.Println("Error2")
+		return
+	}
+	errUnmarshall2 := json.Unmarshal(body_Artists, &jsonList_Artists)
+	if errUnmarshall2 != nil {
+		fmt.Println("Error3")
 		return
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // Lunch a new page for the lose condition
 		tHome := template.Must(template.ParseFiles("./templates/home.html")) // Read the home page
-		tHome.Execute(w, jsonList)
+		tHome.Execute(w, jsonList_Artists)
 	})
 
 	http.HandleFunc("/artistes", func(w http.ResponseWriter, r *http.Request) {
@@ -75,4 +110,6 @@ func main() {
 
 	fmt.Println("http://localhost:8888") // Creat clickable link in the terminal
 	http.ListenAndServe(port, nil)
+
+
 }
