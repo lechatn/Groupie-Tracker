fdd875d861ad42ecb3cc06420d71839c733a8e87
lechatn <noe.lechat@ynov.com>
2024-02-20 10:15:12 +0100

    Ajout des commentaires et supresssion de code inutile

--- Server/filter.go
+++ Server/filter.go
@@ -1,5 +1,6 @@
+//Define the package Server
 package Server
-
+//Import the used packages
 import (
 	"Groupie/structure"
 	"net/http"
@@ -8,21 +9,20 @@ import (
 )
 
 func SearchArtist(w http.ResponseWriter, r *http.Request, jsonList_Artists []structure.Artist, originalData []structure.Artist, lettre string) []structure.Artist {
+	//Function to search an artist by the firsts letters of his group name
 	var new_data []structure.Artist
-	//fmt.Println(lettre)
 	if lettre == "" {
 		return jsonList_Artists
 	}
-	if len(originalData) != len(jsonList_Artists) {
-		//fmt.Println(originalData)
+	if len(originalData) != len(jsonList_Artists) { // If the user has already made a search, we reset the data
 		jsonList_Artists = originalData
 	}
-	if strings.ToUpper(lettre) == "ALL" {
+	if strings.ToUpper(lettre) == "ALL" { // If the user wants to see all the artists, he can type "all" in the search bar or click on the "view all artists" button	
 		return originalData
 	}
 	for i := 0; i < len(jsonList_Artists); i++ {
 		for j := 0; j < len(lettre); j++ {
-			if strings.ToUpper(string(jsonList_Artists[i].Name[j])) == strings.ToUpper(string(lettre[j])) {
+			if strings.ToUpper(string(jsonList_Artists[i].Name[j])) == strings.ToUpper(string(lettre[j])) { // We compare the firts letter of the group name with the first letter of the client search
 				if j == len(lettre)-1 {
 					new_data = append(new_data, jsonList_Artists[i])
 				} else {
@@ -38,46 +38,47 @@ func SearchArtist(w http.ResponseWriter, r *http.Request, jsonList_Artists []str
 			}
 		}
 	}
-	return new_data
+	return new_data // We return the new data with the artists that match with the client search
 }
 
 func SortData(w http.ResponseWriter, r *http.Request, jsonList_Artists []structure.Artist) []structure.Artist {
-	order1 := r.FormValue("alpha")
-	order2 := r.FormValue("unalpha")
-	order3 := r.FormValue("firstalbum")
-	order4 := r.FormValue("CreationDate")
-	order5 := r.FormValue("mostArtists")
-	order6 := r.FormValue("lessArtists")
-	if order1 == "" && order2 == "" && order3 == "" && order4 == "" && order5 == "" && order6 == ""{
+	//Function to sort the data with 6 different possibilities
+	order1 := r.FormValue("alpha") // Sort in alphabetical order
+	order2 := r.FormValue("unalpha") // Sort in reverse alphabetical order
+	order3 := r.FormValue("firstalbum") // Sort by the date of the first album
+	order4 := r.FormValue("CreationDate") // Sort by the creation date of the group
+	order5 := r.FormValue("mostArtists") // Sort by the greatest number of members
+	order6 := r.FormValue("lessArtists") // Sort by the smallest number of members
+	if order1 == "" && order2 == "" && order3 == "" && order4 == "" && order5 == "" && order6 == ""{ // If the client doesn't want to sort the data, we return the data as it is
 		return jsonList_Artists
 	}
-	if order1 != "" {
-		sort.Slice(jsonList_Artists, func(i, j int) bool {
+	if order1 != "" { // If the client choose the firts option, we sort the data in alphabetical order
+		sort.Slice(jsonList_Artists, func(i, j int) bool { // We use the sort.Slice function to sort the data
 			return strings.ToUpper(jsonList_Artists[i].Name) < strings.ToUpper(jsonList_Artists[j].Name)
 		})
-	} else if order2 != "" {
+	} else if order2 != "" { // If the client choose the second option, we sort the data in reverse alphabetical order
 		sort.Slice(jsonList_Artists, func(i, j int) bool {
 			return strings.ToUpper(jsonList_Artists[i].Name) > strings.ToUpper(jsonList_Artists[j].Name)
 		})
-	} else if order3 != "" {
+	} else if order3 != "" { // If the client choose the third option, we sort the data by the date of the first album
 		sort.Slice(jsonList_Artists, func(i, j int) bool {
-			if jsonList_Artists[i].FirstAlbum[6:] == jsonList_Artists[j].FirstAlbum[6:] {
-				if jsonList_Artists[i].FirstAlbum[3:5] == jsonList_Artists[j].FirstAlbum[3:5] {
-					return jsonList_Artists[i].FirstAlbum[:2] < jsonList_Artists[j].FirstAlbum[:2]
+			if jsonList_Artists[i].FirstAlbum[6:] == jsonList_Artists[j].FirstAlbum[6:] { // Firstly, we compare the year of the first album
+				if jsonList_Artists[i].FirstAlbum[3:5] == jsonList_Artists[j].FirstAlbum[3:5] { // Secondly, we compare the month of the first album
+					return jsonList_Artists[i].FirstAlbum[:2] < jsonList_Artists[j].FirstAlbum[:2] // Finally, we compare the day of the first album
 				}
 				return jsonList_Artists[i].FirstAlbum[3:5] < jsonList_Artists[j].FirstAlbum[3:5]
 			}
 			return jsonList_Artists[i].FirstAlbum[6:] < jsonList_Artists[j].FirstAlbum[6:]
 		})
-	} else if order4 != "" {
+	} else if order4 != "" { // If the client choose the fourth option, we sort the data by the creation date of the group
 		sort.Slice(jsonList_Artists, func(i, j int) bool {
 			return jsonList_Artists[i].CreationDate < jsonList_Artists[j].CreationDate
 		})
-	} else if order5 != "" {
+	} else if order5 != "" { // If the client choose the fifth option, we sort the data by the greatest number of members
 		sort.Slice(jsonList_Artists, func(i, j int) bool {
 			return len(jsonList_Artists[i].Members) > len(jsonList_Artists[j].Members)
 		})
-	} else if order6 != "" {
+	} else if order6 != "" { // If the client choose the sixth option, we sort the data by the smallest number of members
 		sort.Slice(jsonList_Artists, func(i, j int) bool {
 			return len(jsonList_Artists[i].Members) < len(jsonList_Artists[j].Members)
 		})
--- Server/loadApi.go
+++ Server/loadApi.go
@@ -1,5 +1,7 @@
+// Define the package Server
 package Server
 
+//Import the used packages
 import (
 	"Groupie/structure"
 	"encoding/json"
@@ -7,152 +9,121 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 	"text/template"
 )
 
 func LoadArtistes(w http.ResponseWriter, r *http.Request) []structure.Artist {
-	url_Artists := "https://groupietrackers.herokuapp.com/api/artists"
+	//Function to load the page "artists" from the API
+	url_Artists := "https://groupietrackers.herokuapp.com/api/artists" // We define the good URL
 
 	var jsonList_Artists []structure.Artist
 	response_Artists, err := http.Get(url_Artists)
+	// Management of the error
 	if err != nil {
-		fmt.Println("Erreur de connexion a l'API :", err)
+		fmt.Println("Erreur de connexion a l'API 'artists' :", err)
 		os.Exit(0)
 	}
 
-	defer response_Artists.Body.Close()
+	defer response_Artists.Body.Close() // We wait for the end of the function to close the body
 
-	body_Artists, err := io.ReadAll(response_Artists.Body)
+	body_Artists, err := io.ReadAll(response_Artists.Body) // We read the body of the API
+	// Management of the error
 	if err != nil {
-		fmt.Println("Erreur de lecture de l'API :", err)
+		fmt.Println("Erreur de lecture de l'API 'artists' :", err)
 		os.Exit(0)
 	}
-	errUnmarshall1 := json.Unmarshal(body_Artists, &jsonList_Artists)
+	errUnmarshall1 := json.Unmarshal(body_Artists, &jsonList_Artists) // We decode the result with function Unmarshall
+	// Management of the error
 	if errUnmarshall1 != nil {
-		fmt.Println("Erreur de décodage de l'API :", errUnmarshall1)
+		fmt.Println("Erreur de décodage de l'API 'artists' :", errUnmarshall1)
 		os.Exit(0)
 	}
 	return jsonList_Artists
 
 }
 
-func LoadLocation(w http.ResponseWriter, r *http.Request, data []structure.Artist) {
-	//res := make([]map[string][]string, len(data))
-	for i := 0; i < len(data); i++ {
-		url_location := "https://groupietrackers.herokuapp.com/api/relation/" + strconv.Itoa(data[i].IdArtists)
-
-		response_location, err := http.Get(url_location)
-		if err != nil {
-			fmt.Println("Erreur de connexion à l'API :", err)
-			os.Exit(0)
-		}
-
-		defer response_location.Body.Close()
-
-		body_location, err := io.ReadAll(response_location.Body)
-		if err != nil {
-			fmt.Println("Erreur de lecture de l'API :", err)
-			os.Exit(0)
-		}
-
-		var json_location structure.Relations
-		errUnmarshall2 := json.Unmarshal(body_location, &json_location)
-		if errUnmarshall2 != nil {
-			fmt.Println("Erreur de décodage de l'API :", errUnmarshall2)
-			os.Exit(0)
-		}
-
-	}
-
-}
-
 func LoadRelation(w http.ResponseWriter, r *http.Request, id string, infos_artist structure.Artist) structure.Relations {
-
+	// Function to load the page "relation" from the API
 	var json_Relation structure.Relations
 
-	url_Relations := "https://groupietrackers.herokuapp.com/api/relation/" + id
+	url_Relations := "https://groupietrackers.herokuapp.com/api/relation/" + id // We define the good URL with the id selected by the client
 
 	response_Relations, err := http.Get(url_Relations)
+	// Management of the error
 	if err != nil {
-		fmt.Println("Erreur de connexion à l'API :", err)
+		fmt.Println("Erreur de connexion à l'API 'relation' :", err)
 		os.Exit(0)
 	}
 
 	defer response_Relations.Body.Close()
 
 	body_Relations, err := io.ReadAll(response_Relations.Body)
+	// Management of the error
 	if err != nil {
-		fmt.Println("Erreur de lecture de l'API :", err)
+		fmt.Println("Erreur de lecture de l'API 'relation' :", err)
 		os.Exit(0)
 	}
 
 	//fmt.Println(body_Relations)
 
 	errUnmarshall3 := json.Unmarshal(body_Relations, &json_Relation)
+	// Management of the error
 	if errUnmarshall3 != nil {
-		fmt.Println("Erreur de décodage de l'API :", errUnmarshall3)
+		fmt.Println("Erreur de décodage de l'API 'relation' :", errUnmarshall3)
 		os.Exit(0)
 	}
 
-	data := structure.Relations{}
+	data := structure.Relations{} // We create a new structure to stock the data
 	data.Id = json_Relation.Id
 	data.DateLocation = json_Relation.DateLocation
 	data.Infos = infos_artist
-	data.Coordonnees = SearchLatLon(data.DateLocation)
+	data.Coordonnees = SearchLatLon(data.DateLocation) // We call the function SearchLatLon to get the latitude and longitude of all the cities of DateLocation
 
 	tRelation := template.Must(template.ParseFiles("./templates/relation.html")) // Read the relation page
-	//fmt.Println(data)
 	tRelation.Execute(w, data)
-	//fmt.Println(data)
 	return data
 }
 
 func SearchLatLon(relation map[string][]string) map[string][]string {
-	res := make(map[string][]string, len(relation))
+	res := make(map[string][]string, len(relation)) // We create a map to stock the latitude and longitude for the javascript file
 	url := ""
 	for city := range relation {
-		city = strings.ReplaceAll(city, "-", ",")
-		if city == "willemstad,netherlands_antilles" {
+		city = strings.ReplaceAll(city, "-", ",")      // We replace the "-" by "," to avoid the error of the API nominatim
+		if city == "willemstad,netherlands_antilles" { // We have to make a special case for the city "willemstad" because the API doesn't recognize the city "willemstad,netherlands_antilles"
 			url = "https://nominatim.openstreetmap.org/search?q=willemstad&format=json"
 		} else {
-			url = "https://nominatim.openstreetmap.org/search?q=" + city + "&format=json"
-		}
-
-		if city == "willemstad,netherlands_antilles" {
-			url = "https://nominatim.openstreetmap.org/search?q=willemstad&format=json"
-		} else {
-			url = "https://nominatim.openstreetmap.org/search?q=" + city + "&format=json"
+			url = "https://nominatim.openstreetmap.org/search?q=" + city + "&format=json" // Else, we define the good URL by using the city
 		}
 
 		response, err := http.Get(url)
-
+		// Management of the error
 		if err != nil {
-			fmt.Println("Erreur de connexion à l'API :", err)
+			fmt.Println("Erreur de connexion à l'API nominatim :", err)
 			os.Exit(0)
 		}
-
 		defer response.Body.Close()
 
 		body, err := io.ReadAll(response.Body)
+		// Management of the error
 		if err != nil {
-			fmt.Println("Ereur de lecture de l'API :", err)
+			fmt.Println("Ereur de lecture de l'API 'nominatim' :", err)
 			os.Exit(0)
 		}
 
 		var data []structure.Place
 		errUnmarshall4 := json.Unmarshal(body, &data)
+		// Management of the error
 		if errUnmarshall4 != nil {
-			fmt.Println("Erreur de décodage de l'API :", errUnmarshall4)
+			fmt.Println("Erreur de décodage de l'API 'nominatim' :", errUnmarshall4)
 			os.Exit(0)
 		}
 
 		var inter []string
-		inter = append(inter, data[0].Lat)
+		inter = append(inter, data[0].Lat) // We stock the latitude and longitude in an intermediate slice
 		inter = append(inter, data[0].Lon)
-		res[city] = inter
-		inter = nil
+		res[city] = inter // We stock the intermediate slice in the result map
+		inter = nil // We reset the intermediate slice
 
 	}
 	return res
