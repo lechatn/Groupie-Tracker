ab9ac6ca70fdaf3c7cc3dcd4d0d20536e57541f4
cnino <nino.carlotti@ynov.com>
2024-02-22 10:10:20 +0100

    debug

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Define the package main 
+// Define the package main
 package main
 
 // Import the used packages
@@ -12,7 +12,6 @@ import (
 	"sort"
 	"strconv"
 )
-
 
 var jsonList_Artists []structure.Artist
 
@@ -28,11 +27,10 @@ var data_artist structure.Relations
 
 var infos_artist structure.Artist
 
-
 func main() {
-	css := http.FileServer(http.Dir("style"))                
-	http.Handle("/style/", http.StripPrefix("/style/", css)) 
-	img := http.FileServer(http.Dir("images"))              
+	css := http.FileServer(http.Dir("style"))
+	http.Handle("/style/", http.StripPrefix("/style/", css))
+	img := http.FileServer(http.Dir("images"))
 	http.Handle("/images/", http.StripPrefix("/images/", img))
 	js := http.FileServer(http.Dir("js"))
 	http.Handle("/js/", http.StripPrefix("/js/", js))
@@ -42,20 +40,20 @@ func main() {
 		tHome.Execute(w, nil)
 	})
 
-	http.HandleFunc("/artistes", func(w http.ResponseWriter, r *http.Request) { // Define the path for the artists page
+	http.HandleFunc("/artists", func(w http.ResponseWriter, r *http.Request) { // Define the path for the artists page
 		if !artist_create { // If the artist list is not created, we create it. If it's already created, we don't create it again to avoid to overload the API
 			jsonList_Artists = Server.LoadArtistes(w, r)
 			artist_create = true
 			originalData = jsonList_Artists
 		}
-		tArtistes := template.Must(template.ParseFiles("./templates/artistes.html"))
+		tArtistes := template.Must(template.ParseFiles("./templates/artists.html"))
 		if r.FormValue("Check") != "" { // If the client type a letter in the search bar, we call the function SearchArtist to filter the data
 			lettre := r.FormValue("Check")
 			jsonList_Artists = Server.SearchArtist(w, r, jsonList_Artists, originalData, lettre)
 			lettre = ""
 			if len(jsonList_Artists) == 0 {
 				terror := template.Must(template.ParseFiles("./templates/error.html"))
-				terror.Execute(w,nil)
+				terror.Execute(w, nil)
 				jsonList_Artists = originalData
 				return
 			}
@@ -77,7 +75,7 @@ func main() {
 	http.HandleFunc("/loading", func(w http.ResponseWriter, r *http.Request) { // Define the path for the loading page
 		tloading := template.Must(template.ParseFiles("./templates/loading.html"))
 		id = r.URL.Query().Get("id") // We get the id selected by the client
-		redirect := false 
+		redirect := false
 		go func() { // We use a goroutine to load the data and start the loading page in the same time
 			id_int, _ := strconv.Atoi(id)
 			sort.Slice(originalData, func(i, j int) bool {
@@ -102,10 +100,10 @@ func main() {
 
 	http.HandleFunc("/relationForJs", func(w http.ResponseWriter, r *http.Request) { // Define the path for the recup of the data by the javascript file
 		json.NewEncoder(w).Encode(data_artist) // We encode the data to send it to the javascript file
-		data_artist = structure.Relations{} // We reset the data to aavoid some problems
+		data_artist = structure.Relations{}    // We reset the data to aavoid some problems
 	})
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static")))) 
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	fmt.Println("http://localhost:8768") // Creat clickable link in the terminal
 	http.ListenAndServe(port, nil)
